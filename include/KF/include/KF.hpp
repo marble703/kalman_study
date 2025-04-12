6b91286cb24b90266089fd1e376f9bbf6f9ebf26
marble703 <703342335@qq.com>
2025-04-12 19:37:50 +0800

    Feat(KF)完成KF主体代码

--- include/KF/include/KF.hpp
+++ include/KF/include/KF.hpp
@@ -1,52 +1,126 @@
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/src/Core/Matrix.h>
+#include <eigen3/Eigen/src/Core/util/Constants.h>
 
-template<typename T>
 class KF {
 public:
     /**
-     * @brief Construct a new KF object
+     * @brief 完整模型构造,不初始化dt
      * 
-     * @param f  // 状态转移矩阵
-     * @param h  // 观测矩阵
-     * @param b  // 控制输入矩阵
-     * @param q  // 过程噪声协方差矩阵
-     * @param r  // 观测噪声协方差矩阵
-     * @param p // 估计误差协方差矩阵
+     * @param f // 状态转移矩阵(固定)
+     * @param h // 观测矩阵(固定)
+     * @param b // 控制输入矩阵(固定)
+     * @param q // 过程噪声协方差矩阵(可变)
+     * @param r // 观测噪声协方差矩阵(可变)
      */
-    KF(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> f,
-       Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> h,
-       Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> b,
-       Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> q,
-       Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> r,
-       Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> p);
+    KF(const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& f,
+       const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& h,
+       const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& b,
+       const Eigen::Ref<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& q,
+       const Eigen::Ref<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& r);
     KF(const KF& kf);
     ~KF();
 
-    void init(Eigen::Vector<T, Eigen::Dynamic> initState // 初始状态
-    );
-    void predict();
-    void update(float measurement, float dt);
+    /**
+     * @brief 传入初始状态,初始化卡尔曼滤波器
+     * @param initState 初始状态
+     */
+    void
+    init(const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+             initState);
 
-    float getState() const;
+    /**
+     * @brief 使用观察值和时间间隔更新卡尔曼滤波器,预测下一帧,并更新卡尔曼增益矩阵
+     * @brief 使用默认观测矩阵h
+     * @param measurement 观察值
+     * @param dt 时间间隔
+     */
+    void update(
+        const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+            measurement,
+        float dt
+    );
+
+    void
+    update(const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+               measurement);
+
+    /**
+     * @brief 使用观测值更新卡尔曼滤波器
+     * @brief 使用默认观测矩阵h
+     * @param measurement 观测值
+     */
+    void updateKalmanGain();
+
+    /**
+     * @brief 使用控制量更新卡尔曼滤波器
+     * @brief 使用默认控制输入矩阵b
+     * @param controlInput 控制输入
+     */
+    void
+    control(const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+                controlInput);
+
+    /**
+     * @brief 预测一帧
+     * 
+     */
+    void predict();
+    void predict(float dt);
+
+    void resetState();
+    void resetState(
+        const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+            measurement
+    );
+
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> getState() const;
 
 private:
-    void resetState();
+    // 矩阵维度
+    size_t ObservationSize_; // 观测维度
+    size_t StateSize_;       // 状态维度
+    size_t ControlSize_;     // 控制输入维度
 
-    void setMeasurement(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> measurement);
+    // 模型矩阵(固定)
+    const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+        f_; // 状态转移矩阵,大小为StateSize_ * StateSize_
+    const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+        h_; // 观测矩阵,大小为ObservationSize_ * StateSize_
+    const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+        b_; // 控制输入矩阵,大小为StateSize_ * ControlSize_
 
-    void setControlInput(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> controlInput);
+    // 初始量
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+        initState_; // 初始状态矩阵,大小为StateSize_
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+        p0_; // 初始协方差矩阵,大小为StateSize_ * StateSize_
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+        q0_; // 初始过程噪声协方差矩阵,大小为StateSize_ * StateSize_
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+        r0_; // 初始观测噪声协方差矩阵,大小为ObservationSize_ * ObservationSize_
 
-    float getPredictedState() const;
+    // 噪声协方差矩阵
+    const Eigen::Ref<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+        q_; // 过程噪声协方差矩阵,大小为StateSize_ * StateSize_
+    const Eigen::Ref<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+        r_; // 观测噪声协方差矩阵,大小为ObservationSize_ * ObservationSize_
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+        k_; // 卡尔曼增益矩阵,大小为StateSize_ * ObservationSize_
 
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> f_; // 状态转移矩阵
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> h_; // 观测矩阵
-    
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> b_; // 控制输入矩阵
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> q_; // 过程噪声协方差矩阵
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> r_; // 观测噪声协方差矩阵
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> p_; // 估计误差协方差矩阵
+    // 当前量
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+        current_state_; // 当前状态矩阵,大小为StateSize_*StateSize_
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+        controlInput_; // 控制输入矩阵,大小为ControlSize_
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+        measurement_; // 观测值矩阵,大小为ObservationSize_
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+        KalmanGain_; // 卡尔曼增益矩阵,大小为StateSize_ * ObservationSize_
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+        p_; // 协方差矩阵,大小为StateSize_ * StateSize_
+    // 预测量
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> predictedState_; // 预测状态矩阵
 
-    Eigen::Vector<T, Eigen::Dynamic> initState_; // 初始状态
-    Eigen::Vector<T, Eigen::Dynamic> state_; // 当前状态
+    double dt_; // 时间间隔
 };
--- include/KF/src/KF.cpp
+++ include/KF/src/KF.cpp
@@ -1,20 +1,105 @@
 #include "KF/include/KF.hpp"
 
-template<typename T>
-KF<T>::KF(
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> f,
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> h,
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> b,
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> q,
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> r,
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> p
-) : f_(f), h_(h), b_(b), q_(q), r_(r), p_(p) {
+KF::KF(
+    const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& f,
+    const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& h,
+    const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& b,
+    const Eigen::Ref<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& q,
+    const Eigen::Ref<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& r
+):
+    f_(f),
+    h_(h),
+    b_(b),
+    q_(q),
+    r_(r) {
     assert(f_.rows() == f_.cols() && "状态转移矩阵 f 必须是方阵");
     assert(h_.rows() == h_.cols() && "观测矩阵 h 必须是方阵");
     assert(b_.rows() == b_.cols() && "控制输入矩阵 b 必须是方阵");
     assert(q_.rows() == q_.cols() && "过程噪声协方差矩阵 q 必须是方阵");
     assert(r_.rows() == r_.cols() && "观测噪声协方差矩阵 r 必须是方阵");
-    assert(p_.rows() == p_.cols() && "初始估计协方差矩阵 p 必须是方阵");
-    assert(f_.rows() == h_.rows() && f_.rows() == b_.rows() && f_.rows() == q_.rows() && f_.rows() == r_.rows() && f_.rows() == p_.rows() && "矩阵维度不匹配");
-    assert(f_.cols() == h_.cols() && f_.cols() == b_.cols() && f_.cols() == q_.cols() && f_.cols() == r_.cols() && f_.cols() == p_.cols() && "矩阵维度不匹配");
+    assert(
+        f_.rows() == h_.rows() && f_.rows() == b_.rows() && f_.rows() == q_.rows()
+        && f_.rows() == r_.rows() && "矩阵维度不匹配"
+    );
+    assert(
+        f_.cols() == h_.cols() && f_.cols() == b_.cols() && f_.cols() == q_.cols()
+        && f_.cols() == r_.cols() && "矩阵维度不匹配"
+    );
+}
+
+void KF::init(const Eigen::Ref<
+              const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>& initState) {
+    this->initState_ = initState;
+    this->current_state_ = this->initState_;
+}
+
+void KF::update(
+    const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+        measurement
+) {
+    this->measurement_ = measurement;
+
+    predict();
+    updateKalmanGain();
+}
+
+void KF::update(
+    const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+        measurement,
+    float dt
+) {
+    this->measurement_ = measurement;
+    this->dt_ = dt;
+
+    predict(this->dt_);
+    updateKalmanGain();
+}
+
+void KF::predict() {
+    // 预测状态
+    this->current_state_ = f_ * this->current_state_ * this->dt_ + b_ * this->controlInput_;
+    // 预测协方差矩阵
+    p_ = f_ * p_ * f_.transpose() + q_;
+}
+void KF::predict(float dt) {
+    this->dt_ = dt;
+    // 预测状态
+    this->predict();
+}
+
+void KF::updateKalmanGain() {
+    // 计算卡尔曼增益矩阵
+    this->k_ = p_ * h_.transpose() * (h_ * p_ * h_.transpose() + r_).inverse();
+    // 更新状态
+    this->current_state_ = this->current_state_
+        + this->k_ * (this->measurement_ - h_ * this->current_state_);
+    // 更新协方差矩阵
+    p_ = (Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>::Identity(
+              f_.rows(),
+              f_.cols()
+          )
+          - this->k_ * h_)
+        * p_;
+}
+
+void KF::control(
+    const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+        controlInput
+) {
+    this->controlInput_ = controlInput;
+    this->current_state_ = f_ * this->current_state_ + b_ * this->controlInput_;
+}
+
+void KF::resetState() {
+    this->current_state_ = this->initState_;
+}
+void KF::resetState(
+    const Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>&
+        measurement
+) {
+    this->current_state_ = measurement;
+}
+
+Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> KF::getState() const {
+    return this->current_state_;
 }
