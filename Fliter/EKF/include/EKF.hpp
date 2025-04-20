87a43836c5189104b9f142e4c9231f81da675fa3
marble703 <703342335@qq.com>
2025-04-20 16:40:20 +0800

    refactor(EKF): 重构EKF类，接口与KF类统一

--- Fliter/EKF/include/EKF.hpp
+++ Fliter/EKF/include/EKF.hpp
@@ -1,71 +1,152 @@
+#pragma once
+
 #include <eigen3/Eigen/Dense>
+#include <optional>
+#include <iostream>
 
-class EKF {
+#include "Utils/BindMatrix.hpp"
+
+#include "FilterBase.hpp"
+
+class EKF: public FilterBase {
 public:
+    /**
+     * @brief 完整构造
+     * 
+     * @param f  // 状态转移矩阵(固定)
+     * @param h  // 观测矩阵(固定)
+     * @param b  // 控制输入矩阵(固定)
+     * @param q  // 过程噪声协方差矩阵(固定)
+     * @param r  // 观测噪声协方差矩阵(固定)
+     * @param dt // 时间间隔(固定)
+     */
     EKF(const Eigen::MatrixXd f,
-        const Eigen::MatrixXd h,
-        const Eigen::MatrixXd b,
-        Eigen::MatrixXd q,
-        Eigen::MatrixXd r,
-        double dt = 0.01);
+       const Eigen::MatrixXd h,
+       const Eigen::MatrixXd b,
+       const Eigen::MatrixXd q,
+       const Eigen::MatrixXd r,
+       double dt = 0.0);
 
-    EKF(Eigen::MatrixXd f,
-        Eigen::MatrixXd h,
-        Eigen::MatrixXd b);
+    /**
+     * @brief 动态 dt 构造
+     * 
+     * @param f  // 状态转移矩阵(固定)
+     * @param h  // 观测矩阵(固定)
+     * @param b  // 控制输入矩阵(固定)
+     * @param q  // 过程噪声协方差矩阵(固定)
+     * @param r  // 观测噪声协方差矩阵(固定)
+     * @param dt // 时间间隔(可变)
+     */
+    EKF(utils::BindableMatrixXd f,
+       const Eigen::MatrixXd h,
+       const Eigen::MatrixXd b,
+       const Eigen::MatrixXd q,
+       const Eigen::MatrixXd r);
 
-    EKF(const EKF& ekf);
+    /**
+    * @brief 仅模型构造,仅初始化模型
+    * 
+    * @param f // 状态转移矩阵(固定)
+    * @param h // 观测矩阵(固定)
+    * @param b // 控制输入矩阵(固定)
+    */
+    EKF(const Eigen::MatrixXd f, const Eigen::MatrixXd h, const Eigen::MatrixXd b, double dt = 0.0);
 
-    EKF& operator=(const EKF& ekf);
+    /**
+     * @brief 拷贝构造函数,只拷贝模型
+     */
+    EKF(const EKF& EKF);
+
+    /**
+     * @brief 拷贝赋值函数,复制所有成员变量
+     * @param EKF 需要复制的EKF对象
+     * @return 返回当前对象的引用
+     */
+    EKF& operator=(const EKF& EKF);
 
     EKF() = default;
 
     ~EKF() = default;
 
-    void init(const Eigen::MatrixXd& initState);
+    /**
+     * @brief 传入初始状态,初始化卡尔曼滤波器
+     * @param initState 初始状态
+     */
+    void init(const Eigen::MatrixXd& initState) override;
 
-    void update(const Eigen::MatrixXd& measurement, float dt);
+    /**
+     * @brief 使用观测值和时间间隔更新卡尔曼滤波器,预测下一帧,并更新卡尔曼增益矩阵
+     * @note 使用默认观测矩阵h
+     * @param measurement 观测值
+     * @param dt 时间间隔
+     */
+    void
+    update(const Eigen::MatrixXd& measurement, double dt = 0.0, bool setDefault = false) override;
 
-    void update(const Eigen::MatrixXd& measurement);
+    /**
+     * @brief 使用控制量更新卡尔曼滤波器
+     * @note 使用默认控制输入矩阵b
+     * @param controlInput 控制输入
+     */
+    void control(const Eigen::MatrixXd& controlInput, double dt = 0.0) override;
 
-    void updateKalmanGain();
+    /**
+     * @brief 使用传入的时间间隔预测一帧
+     * @param dt 时间间隔
+     */
+    void predict(double dt = 0.0) override;
 
-    void control(const Eigen::MatrixXd& controlInput);
+    /**
+     * @brief 重置状态
+     * @param currentState 设置模型状态矩阵
+     */
+    void resetState(const Eigen::MatrixXd& currentState = Eigen::MatrixXd()) override;
 
-    void predict(float dt);
-
-    void predict();
-
-    void resetState(const Eigen::MatrixXd& currentState);
-
-    void resetState();
-
-    Eigen::MatrixXd getState() const;
+    /**
+     * @brief 获取当前状态
+     * @return 当前状态矩阵
+     */
+    Eigen::MatrixXd getState() const override;
 
 private:
-    size_t ObservationSize_;
-    size_t StateSize_;
-    size_t ControlSize_;
+    /**
+     * @brief 使用观测值更新卡尔曼滤波器
+     * @param measurement 观测值
+     */
+    void updateKalmanGain();
 
-    const Eigen::MatrixXd f_;
-    const Eigen::MatrixXd h_;
-    const Eigen::MatrixXd b_;
+    void initandCheck();
 
-    Eigen::Matrix<double, Eigen::Dynamic, 1> initState_;
-    Eigen::MatrixXd p0_;
-    Eigen::MatrixXd q0_;
-    Eigen::MatrixXd r0_;
+    // 矩阵维度
+    int ObservationSize_; // 观测维度
+    int StateSize_;       // 状态维度
+    int ControlSize_;     // 控制输入维度
 
-    Eigen::MatrixXd q_;
-    Eigen::MatrixXd r_;
-    Eigen::MatrixXd k_;
+    // 模型矩阵(固定)
+    const utils::BindableMatrixXd f_; // 状态转移矩阵,大小为StateSize_ * StateSize_
+    const Eigen::MatrixXd h_;         // 观测矩阵,大小为ObservationSize_ * StateSize_
+    const Eigen::MatrixXd b_;         // 控制输入矩阵,大小为StateSize_ * ControlSize_
 
-    Eigen::MatrixXd current_state_;
-    Eigen::Matrix<double, Eigen::Dynamic, 1> controlInput_;
-    Eigen::Matrix<double, Eigen::Dynamic, 1> measurement_;
-    Eigen::MatrixXd KalmanGain_;
-    Eigen::MatrixXd p_;
+    // 初始量
+    Eigen::Matrix<double, Eigen::Dynamic, 1> initState_; // 初始状态矩阵,大小为StateSize_ * 1
+    Eigen::MatrixXd p0_; // 初始协方差矩阵,大小为StateSize_ * StateSize_
+    Eigen::MatrixXd q0_; // 初始过程噪声协方差矩阵,大小为StateSize_ * StateSize_
+    Eigen::MatrixXd r0_; // 初始观测噪声协方差矩阵,大小为ObservationSize_ * ObservationSize_
 
-    Eigen::MatrixXd predictedState_;
+    // 噪声协方差矩阵
+    Eigen::MatrixXd q_; // 过程噪声协方差矩阵,大小为StateSize_ * StateSize_
+    Eigen::MatrixXd r_; // 观测噪声协方差矩阵,大小为ObservationSize_ * ObservationSize_
+    Eigen::MatrixXd k_; // 卡尔曼增益矩阵,大小为StateSize_ * ObservationSize_
 
-    double dt_;
+    // 当前量
+    Eigen::MatrixXd current_state_; // 当前状态矩阵,大小为StateSize_*1
+    Eigen::Matrix<double, Eigen::Dynamic, 1> controlInput_; // 控制输入矩阵,大小为ControlSize_ * 1
+    Eigen::Matrix<double, Eigen::Dynamic, 1> measurement_; // 观测值矩阵,大小为ObservationSize_ * 1
+    Eigen::MatrixXd KalmanGain_; // 卡尔曼增益矩阵,大小为StateSize_ * ObservationSize_
+    Eigen::MatrixXd p_;          // 协方差矩阵,大小为StateSize_ * StateSize_
+
+    // 预测量
+    Eigen::MatrixXd predictedState_; // 预测状态矩阵
+
+    double dt_; // 时间间隔
 };
