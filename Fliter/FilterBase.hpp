819e5f60ac20105d4235fe5cb671cf6c11103edd
marble703 <703342335@qq.com>
2025-04-23 16:23:55 +0800

    refactor(FilterBase): 移除未使用成员变量

--- Fliter/FilterBase.hpp
+++ Fliter/FilterBase.hpp
@@ -27,38 +27,4 @@ public:
 
     // 获取当前状态
     virtual Eigen::MatrixXd getState() const = 0;
-
-protected:
-    // 矩阵维度
-    int ObservationSize_; // 观测维度
-    int StateSize_;       // 状态维度
-    int ControlSize_;     // 控制输入维度
-
-    // 模型矩阵(固定)
-    const utils::BindableMatrixXd f_; // 状态转移矩阵,大小为StateSize_ * StateSize_
-    const Eigen::MatrixXd h_;         // 观测矩阵,大小为ObservationSize_ * StateSize_
-    const Eigen::MatrixXd b_;         // 控制输入矩阵,大小为StateSize_ * ControlSize_
-
-    // 初始量
-    Eigen::Matrix<double, Eigen::Dynamic, 1> initState_; // 初始状态矩阵,大小为StateSize_ * 1
-    Eigen::MatrixXd p0_; // 初始协方差矩阵,大小为StateSize_ * StateSize_
-    Eigen::MatrixXd q0_; // 初始过程噪声协方差矩阵,大小为StateSize_ * StateSize_
-    Eigen::MatrixXd r0_; // 初始观测噪声协方差矩阵,大小为ObservationSize_ * ObservationSize_
-
-    // 噪声协方差矩阵
-    Eigen::MatrixXd q_; // 过程噪声协方差矩阵,大小为StateSize_ * StateSize_
-    Eigen::MatrixXd r_; // 观测噪声协方差矩阵,大小为ObservationSize_ * ObservationSize_
-    Eigen::MatrixXd k_; // 卡尔曼增益矩阵,大小为StateSize_ * ObservationSize_
-
-    // 当前量
-    Eigen::MatrixXd current_state_; // 当前状态矩阵,大小为StateSize_*StateSize_
-    Eigen::Matrix<double, Eigen::Dynamic, 1> controlInput_; // 控制输入矩阵,大小为ControlSize_ * 1
-    Eigen::Matrix<double, Eigen::Dynamic, 1> measurement_; // 观测值矩阵,大小为ObservationSize_ * 1
-    Eigen::MatrixXd KalmanGain_; // 卡尔曼增益矩阵,大小为StateSize_ * ObservationSize_
-    Eigen::MatrixXd p_;          // 协方差矩阵,大小为StateSize_ * StateSize_
-
-    // 预测量
-    Eigen::MatrixXd predictedState_; // 预测状态矩阵
-
-    double dt_; // 时间间隔
 };
